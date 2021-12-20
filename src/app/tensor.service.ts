db5adde3dd3d81bf46817ba59c43a7f77c91b148
Daniel Lahr <daniel.g.lahr@gmail.com>
2021-12-20 15:16:06 +0100

    Tensor normalization for UI

--- src/app/tensor.service.ts
+++ src/app/tensor.service.ts
@@ -17,21 +17,13 @@ export class TensorService {
   constructor() {
   }
 
-  private static imageToTensor(imageBuffer: ArrayBuffer): Tensor3D {
+  private static tiffToTensor(imageBuffer: ArrayBuffer): Tensor3D {
     const image: TiffIfd = tiff.decode(imageBuffer)[0];
-    const tensor = tf.tensor3d(Array.from(image.data), [image.height, image.width, image.get('SamplesPerPixel')], 'float32');
-    const channels: Tensor2D[] = [];
-    for (const channel of tensor.unstack(-1)) {
-      const numerator = channel.sub(channel.min());
-      const denominator = channel.max().sub(channel.min());
-      const c: Tensor2D = numerator.divNoNan(denominator);
-      channels.push(c);
-    }
-    return tf.stack(channels, -1) as Tensor3D;
+    return tf.tensor3d(Array.from(image.data), [image.height, image.width, image.get('SamplesPerPixel')], 'float32');
   }
 
   initializeTensors(imageBuffers: ArrayBuffer[]): void {
-    const tensors = imageBuffers.map(imageBuffer => TensorService.imageToTensor(imageBuffer));
+    const tensors = imageBuffers.map(imageBuffer => TensorService.tiffToTensor(imageBuffer));
     this.numChannels = tensors.flatMap(tensor => tensor.shape[2]).reduce((p, c) => {
       if (p != c) this.tensorSource.error(new Error(`Different channel numbers ${p} and ${c}.`))
       return c
@@ -41,12 +33,23 @@ export class TensorService {
 
   convertToImageData(tensors: Tensor3D[], channelNr: number): Promise<ImageData>[] {
     return tensors.map(tensor => {
-      const channel = tensor.slice([0, 0, channelNr], [tensor.shape[0], tensor.shape[1], 1]);
+      const channel = TensorService.normalize(tensor.slice([0, 0, channelNr], [tensor.shape[0], tensor.shape[1], 1]));
       return browser.toPixels(channel)
         .then(bytes => {
           const [height, width] = channel.shape.slice(0, 2);
           return new ImageData(bytes, width, height);
         });
     });
+  }
+
+  private static normalize(tensor: Tensor3D): Tensor3D {
+    const channels: Tensor2D[] = [];
+    for (const channel of tensor.unstack(-1)) {
+      const numerator = channel.sub(channel.min());
+      const denominator = channel.max().sub(channel.min());
+      const c: Tensor2D = numerator.divNoNan(denominator);
+      channels.push(c);
+    }
+    return tf.stack(channels, -1) as Tensor3D
   }
 }
